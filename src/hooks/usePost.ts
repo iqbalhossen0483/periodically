92337ebf99ffbf752759ca7225bc59bd779a45d6
iqbalhossen0483 <rayhan.senbag.bd@gmail.com>
2022-03-31 19:16:20 +0600

    dfdsf

---
+++ src/hooks/usePost.ts
@@ -0,0 +1,70 @@
+import { useEffect, useState } from "react";
+
+interface Props {
+  pagePost: Post[] | null;
+  pageNumber: number;
+  currentPage: number;
+  handlePagination: (pageNum: number) => void;
+}
+
+function usePost(): Props {
+  const [pagePost, setPagePost] = useState<Post[] | null>(null);
+  const [currentPage, setCurrentPage] = useState<number>(0);
+  const [pageNumber, setPageNumber] = useState<number>(1);
+  const [post, setPost] = useState<Post[] | []>([]);
+
+  //function load data;
+  async function loadData(pageNum: string): Promise<Post[]> {
+    const url: string = `https://hn.algolia.com/api/v1/search_by_date?tags=story&page=${pageNum}`;
+    const res = await fetch(url);
+    const data = await res.json();
+    return data.hits;
+  }
+
+  useEffect(() => {
+    (async () => {
+      let initialData = await loadData("0");
+      setPost(initialData);
+      setPagePost(initialData);
+
+      //call api after every 10 seconds;
+      let pageNum: number = 0;
+      const interval: NodeJS.Timer = setInterval(async () => {
+        pageNum++;
+        const data = await loadData(pageNum.toString());
+
+        // clear the interval after getting all data;
+        if (!data.length) {
+          clearInterval(interval);
+        }
+
+        setPost((prev) => [...prev, ...data]);
+        //page count increase;
+        setPageNumber((prev) => prev + 1);
+      }, 10000);
+    })();
+  }, []);
+
+  //pagination function;
+  function handlePagination(pageNum: number) {
+    const start = pageNum * 20 + 1;
+    const end = start + 20;
+    const pageData = [];
+    for (let index = start; index < end; index++) {
+      if (post[index]) {
+        pageData.push(post[index]);
+      }
+    }
+    setPagePost(pageData);
+    setCurrentPage((prev) => prev + 1);
+  }
+
+  return {
+    pageNumber,
+    pagePost,
+    currentPage,
+    handlePagination,
+  };
+}
+
+export default usePost;
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -7,60 +7,12 @@ import {
   TableHead,
   TableRow,
 } from "@mui/material";
-import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import usePost from "../../hooks/usePost";
 
 function Home() {
-  const [pagePost, setPagePost] = useState<Post[] | null>(null);
-  const [pageNumber, setPageNumber] = useState<number>(1);
-  const [currentPage, setCurrentPage] = useState<number>(0);
-  const [post, setPost] = useState<Post[] | []>([]);
   const navigate = useNavigate();
-
-  async function loadData(pageNum: string): Promise<Post[]> {
-    const url: string = `https://hn.algolia.com/api/v1/search_by_date?tags=story&page=${pageNum}`;
-    const res = await fetch(url);
-    const data = await res.json();
-    return data.hits;
-  }
-
-  useEffect(() => {
-    (async () => {
-      let initialData = await loadData("0");
-      setPost(initialData);
-      setPagePost(initialData);
-
-      //call api after every 10 seconds;
-      let pageNum: number = 0;
-      const interval: NodeJS.Timer = setInterval(async () => {
-        pageNum++;
-        const data = await loadData(pageNum.toString());
-
-        // clear the interval after getting all data;
-        if (!data.length) {
-          clearInterval(interval);
-        }
-
-        setPost((prev) => [...prev, ...data]);
-        //page count increase;
-        setPageNumber((prev) => prev + 1);
-      }, 10000);
-    })();
-  }, []);
-
-  //pagination function;
-  function handlePagination(pageNum: number) {
-    const start = pageNum * 20 + 1;
-    const end = start + 20;
-    const pageData = [];
-    for (let index = start; index < end; index++) {
-      if (post[index]) {
-        pageData.push(post[index]);
-      }
-    }
-    setPagePost(pageData);
-    setCurrentPage((prev) => prev + 1);
-  }
+  const { pageNumber, pagePost, currentPage, handlePagination } = usePost();
 
   const headers: string[] = ["Title", "URL", "Author", "Created_at"];
 
